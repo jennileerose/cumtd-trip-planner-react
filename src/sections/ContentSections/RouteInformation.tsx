16eff456d424f874798af7639ee50bba86ce747f
jennileerose <jennileerbenda@gmail.com>
2024-03-01 18:09:32 -0600

    added static trip and stop time data

--- src/sections/ContentSections/RouteInformation.tsx
+++ src/sections/ContentSections/RouteInformation.tsx
@@ -3,14 +3,15 @@ import Header from '../header'
 import Footer from '../footer'
 import Navigation from '../navigation'
 import React, { useState, useEffect, ReactElement } from 'react'
-import { GET_ROUTES, GET_TRIPS_BY_ROUTE, GET_STOPTIMES_BY_TRIP } from '../../api'
+import { GET_ROUTES } from '../../api'
 import { CUMTDRoute, RouteDetails, TripInfo, StopTimesByTrip, StopPoint } from '../../types'
 import { Container, useColorMode, Select, Flex, Box, Button } from '@chakra-ui/react'
 import { colors } from '../../theme/colors'
+import {getTripData} from './utils'
 
 export default function RouteInformation(): ReactElement {
    const colorMode = useColorMode()
-  const [routes, setRoutes] = useState<CUMTDRoute>()
+   const [routes, setRoutes] = useState<CUMTDRoute>()
    const [selectedRoute, setSelectedRoute] = useState<RouteDetails>({
       routeColor: 'none',
       routeID: 'none',
@@ -19,86 +20,19 @@ export default function RouteInformation(): ReactElement {
       routeTextColor: 'none'
    })
    const data = null
-   const [rawTripData, setRawTripData] = useState()
-   const [tripData, setTripData] = useState<TripInfo[]>()
-   const [rawStopByTripData, setRawStopByTripData] = useState<any>()
-   const [stopsByTripData, setStopsByTripData] = useState()
-
-   const fetchTripsByRoute = () => {
-      fetch(GET_TRIPS_BY_ROUTE + selectedRoute.routeID)
-      .then(resp => resp.json())
-      .then(data => setRawTripData(data))
-   }
-
-   const fetchStopTimesByTrip = (tripID: string) => {
-      fetch(GET_STOPTIMES_BY_TRIP + tripID)
-      .then(resp => resp.json())
-      .then(data => setRawStopByTripData(data))
-   }
-
-   // setting trip IDs into data from JSON string
-   const structureTripData = (data: any) => {
-      const tempTripsArray = [] as TripInfo[]
-      if(data !== null || data !== undefined) {
-         console.log(data.trips)
-         data.trips.forEach((tripData: any) =>{
-            tempTripsArray.push({
-               trip_id: tripData.trip_id,
-               trip_headsign: tripData.trip_headsign,
-               route_id: tripData.route_id,
-               block_id: tripData.block_id,
-               direction: tripData.direction,
-               service_id: tripData.service_id,
-               shape_id: tripData.shape_id
-            })
-         })
-         console.log('temp trip array', tempTripsArray)
-      }
-      else {
-         tempTripsArray.push({
-            trip_id: '',
-            trip_headsign: 'No trips found',
-            route_id: '',
-            block_id: '',
-            direction: '',
-            service_id: '',
-            shape_id: ''
-         })
-      }
-      setTripData(tempTripsArray)
-   }  
-   
-   const getTripIDs = () => {
-      // fetch trips here
-      fetchTripsByRoute()
-      // re-structure the data here
-      if(rawTripData !== undefined) {
-         structureTripData(rawTripData)
-      }
-   }
-   
-   const getStopTimesByTrips = () => {
-      if(tripData !== undefined) {
-         if(tripData[0].trip_id !== '') {
-            getStopTimesByTrips()
-            console.log(tripData)
-            const StopsByTripArray = [] as StopTimesByTrip[]
-            tripData.forEach((trip: TripInfo) => {
-               fetchStopTimesByTrip(trip.trip_id)
-               if (rawStopByTripData !== undefined) {
-                  console.log(rawStopByTripData.stop_times)
-               } else {
-                  console.log('no stop times')
-               }
-            })
-         }
-      }
-   }
+   const [rawTripData, setRawTripData] = useState<any>()
+   // const [tripData, setTripData] = useState<TripInfo[]>()
+   // const [rawStopByTripData, setRawStopByTripData] = useState<any>()
+   // const [stopsByTripData, setStopsByTripData] = useState()
 
    // called via button after selecting a route
    const getTimetableData = () => {
-      getTripIDs()
-      getStopTimesByTrips()
+      // load in trips text file and parse into object array
+      // console.log(tripDataByRouteID[0])
+      console.log(selectedRoute.routeID)
+      const tripData = getTripData(selectedRoute.routeID)
+      // load in stops text file and parse into object array
+      // get trip IDs for route 
    }
 
    const setDataAsType = (data: any) => {
@@ -212,10 +146,10 @@ export default function RouteInformation(): ReactElement {
                <Button variant="primary" onClick={() => getTimetableData()}>Check Data</Button>
                <div id="route-info">
                   <p>{selectedRoute.routeLongName} {selectedRoute.routeShortName}</p>
-                   {rawStopByTripData !== undefined && (
+                   {/*{rawStopByTripData !== undefined && (
                      <p>{JSON.stringify(rawStopByTripData)}</p>
                   )}
-                  {/* {tripData !== undefined && 
+                   {tripData !== undefined && 
                      <>
                         <p>{tripData[0].trip_id}</p>
                         <p>{tripData[0].trip_headsign}</p>
--- src/sections/ContentSections/utils.ts
+++ src/sections/ContentSections/utils.ts
@@ -1,5 +1,7 @@
 import { LatLngExpression } from 'leaflet';
 // import { StopPoint } from '../../types'
+import tripDataByRouteID from '../../staticData/trips.json'
+import stopTimesByTripID from '../../staticData/stop_times.json'
 
 export function getStopCoords(
   stopPoints: any[]
@@ -10,4 +12,31 @@ export function getStopCoords(
     stopCoordinates.push([stop.stop_lat, stop.stop_lon] as LatLngExpression)
   })
   return stopCoordinates
+}
+
+// functions to select from static data trip file
+export function getTripData(routeID: string): any {
+  let data = null
+  const tripArray: any[] = []
+  tripDataByRouteID.forEach((trip) => {
+     if(trip.route_id === routeID) {
+        tripArray.push(trip)
+     }
+  })
+  // console.log(tripArray)
+  // console.log(stopTimesByTripID)
+  const stopArray: any[] = []
+  // tripArray.forEach((routeTrip) => {
+  //   if()
+  // })
+  const stopTimes = stopTimesByTripID as any[]
+  // console.log(stopTimes)
+  stopTimes.forEach((stop) => {
+    if(stop.trip_id === tripArray[0].trip_id) {
+      stopArray.push(stop)
+    }
+  })
+  console.log(stopArray)
+
+  return data
 }
